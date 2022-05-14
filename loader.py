6933601087fc9500c37539edcf6145fc85a0cc1f
unknown <nisargsuthar12@gmail.com>
2022-05-14 16:14:38 +0530

    Implement Loader and Primer

--- loader.py
+++ loader.py
@@ -1,11 +1,32 @@
 import binascii
 from prefetch import *
 
+######################################################################################
+	# TODO: #
+	#########
+	# Fix the corner case for artifact not in database.
+
 def tempLoader():
+	pairlist = []
 	hexbytes = []
-	with open('decomp.pf', 'rb') as f:
+	hexdata = ""
+	asciidata = ""
+	bytecount = 0	
+	with open('test.txt', 'rb') as f:
 		for byte in iter(lambda: f.read(1), b''):
 			hexbytes.append(binascii.hexlify(byte).decode("utf-8"))
+			bytecount += 1
+			asciichar = int.from_bytes(byte, "big")
+			hexdata += binascii.hexlify(byte).decode("utf-8")+" "
+			if asciichar >= 32 and asciichar <= 126:
+				asciidata += chr(asciichar)+" "
+			else:
+				asciidata += ". "
+			if bytecount % 16 == 0:
+				hexdata = hexdata[:-1]
+				hexdata += "\n"
+				asciidata = asciidata[:-1]
+				asciidata += "\n"
 ######################################################################################
 	def isPrefetch():
 		def isCompressed():
@@ -18,8 +39,6 @@ def tempLoader():
 				magic = "".join(hexbytes[b] for b in range(4, 8)).upper()
 				if magic == "53434341": # SCCA
 					print("Prefetch file is not compressed!")
-					# Call Template Generator.
-					prefetch()
 					return True
 				return False
 		return isCompressed()
@@ -40,4 +59,11 @@ def tempLoader():
 			return True
 		return False
 
-	return True if isPrefetch() or isMFT() or isRegistry() else False
+	if isPrefetch():
+		pairlist = prefetch()
+	# elif isMFT():
+	# elif isRegistry():
+	else:
+		pairlist = ["NOT", "FOUND"]
+
+	return True, hexdata, asciidata, pairlist if isPrefetch() or isMFT() or isRegistry() else False, hexdata, asciidata, pairlist
--- veritas.py
+++ veritas.py
@@ -1,82 +1,42 @@
 from kivy.config import Config
 Config.set('graphics', 'window_state', 'maximized')
 
-from colors import *
-from loadtemp import *
+from loader import *
+from primer import *
 import binascii
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 
+######################################################################################
+	# TODO: #
+	#########
+	# Decode for extended ASCII set.
+	# Add a HEX calculator.
+	# Implement FileChooser.
+	# Replace ScrollView with RecycleView.
+	# Implement Tabs.
+	# Write color switcher.
+
 class MainApp(Widget):
 	def openFile(self):
-		hexdata = ""
-		asciidata = ""
-		bytecount = 0
-
-		def colorBytes(view, data, col, pos, howmany):
-			# Function to insert color markup in final string.
-			ogpos = pos
-			oghowmany = howmany
-			print("HOWMANY: ", howmany)
-
-			if view == 0:
-				pos = (pos-1)*3
-				data = data[:pos] + col + data[pos:]
-				# To seek past "[color=xxxxxx]".
-				pos += 14
-				# To seek past the bytes colored.
-				if howmany % 16 == 0:
-					howmany = (howmany-1)*3
-				else:
-					howmany = (howmany-1)*3+2
-				newlinecount = data.count("\n", pos, pos+howmany)
-				print("NEWLINE: ", newlinecount)
-				for seek in range(howmany):
-					pos += 1
-				for newline in range(newlinecount):
-					pos+=1
-				data = data[:pos] + cc + data[pos:]
-			else:
-				pos -= 1
-				data = data[:pos] + col + data[pos:]
-				# To seek past "[color=xxxxxx]".
-				pos+=14
-				newlinecount = data.count("\n", pos, pos+howmany)
-				print("NEWLINE: ", newlinecount)
-				for seek in range(howmany):
-					pos += 1
-				for newline in range(newlinecount):
-					pos+=1
-				data = data[:pos] + cc + data[pos:]
-			print(data)
-			return data
-
-		with open('decomp.pf', 'rb') as f:
-			for byte in iter(lambda: f.read(1), b''):
-				bytecount += 1
-				asciichar = int.from_bytes(byte, "big")
-				hexdata += binascii.hexlify(byte).decode("utf-8")+" "
-
-				if asciichar >= 32 and asciichar <= 126:
-					asciidata += chr(asciichar)
-				else:
-					asciidata += "."
-
-				if bytecount % 16 == 0:
-					hexdata = hexdata[:-1]
-					hexdata += "\n"
-					asciidata += "\n"
-
-		if not tempLoader():
+		temp = tempLoader()
+		if not temp[0]:
 			print("Artifact not found in the current database!")
 			# TODO: Add kivy popup.
-			
-		hexdata = colorBytes(0, hexdata, c["red"], 6, 32)
-		asciidata = colorBytes(1, asciidata, c["red"], 6, 32)
-		
-		self.ids.hex.text = hexdata
-		self.ids.ascii.text = asciidata
+		else:
+			hexdata = temp[1]
+			asciidata = temp[2]
+			pairlist = temp[3]
+			print("PAIRLIST: ", pairlist)
+			for pair in reversed(pairlist):
+				print(pair, ">", pair[0], pair[1])
+				hexdata = colorBytes(0, hexdata, c["red"], pair[0], pair[1])
+				asciidata = colorBytes(1, asciidata, c["red"], pair[0], pair[1])
+
+			self.ids.hex.text = hexdata
+			self.ids.ascii.text =  asciidata.replace(" ", "")
+			self.remove_widget(self.ids.openfile)		
 
 class Veritas(App):
 	def build(self):
